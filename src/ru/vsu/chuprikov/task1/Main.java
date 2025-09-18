ac66e088e1823228b678ede43aea4a4dd0e2bc7d
Den4ikCh <chuprikov.denis.vrn@gmail.com>
2025-09-18 12:13:01 +0300

    Task1 fixes

--- src/ru/vsu/chuprikov/task1/Main.java
+++ src/ru/vsu/chuprikov/task1/Main.java
@@ -9,21 +9,21 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
         System.out.print("Введите радиус меньшей окружности: ");
-        double r1 = in.nextDouble();
+        double radius1 = in.nextDouble();
         System.out.print("Введите радиус средней окружности: ");
-        double r2 = in.nextDouble();
+        double radius2 = in.nextDouble();
         System.out.print("Введите радиус большей окружности: ");
-        double r3 = in.nextDouble();
+        double radius3 = in.nextDouble();
         in.close();
-        System.out.printf("Площадь закрашенной области равна %.5f квадратных единиц.", squareCalculation(r1, r2, r3));
+        System.out.printf("Площадь закрашенной области равна %.5f квадратных единиц.", squareCalculation(radius1, radius2, radius3));
     }
-
-    public static double squareCalculation(double r1, double r2, double r3) {
-        double s1 = (Math.PI * Math.pow(r1, 2)) / 4; //четверть меньшего круга
-        double s2 = (Math.PI * Math.pow(r2, 2) - Math.PI * Math.pow(r1, 2)) * 3 / 8; //фигура, ограниченная средним и малым кругом
-        double s3 = (Math.PI * Math.pow(r3, 2) - Math.PI * Math.pow(r2, 2)) / 2; //фигура, ограниченная большим и средним кругом
-        double s4 = (Math.pow(r3 * 2, 2) - Math.PI * Math.pow(r3, 2)) * 5 / 8; //уголки
-        double res = s1 + s2 + s3 + s4;
-        return res;
+    
+    public static double squareCalculation(double radius1, double radius2, double radius3) {
+        double square1 = (Math.PI * Math.pow(radius1, 2)) / 4; //четверть меньшего круга
+        double square2 = (Math.PI * Math.pow(radius2, 2) - Math.PI * Math.pow(radius1, 2)) * 3 / 8; //фигура, ограниченная средним и малым кругом
+        double square3 = (Math.PI * Math.pow(radius3, 2) - Math.PI * Math.pow(radius2, 2)) / 2; //фигура, ограниченная большим и средним кругом
+        double square4 = (Math.pow(radius3 * 2, 2) - Math.PI * Math.pow(radius3, 2)) * 5 / 8; //уголки
+        double result = square1 + square2 + square3 + square4;
+        return result;
     }
 }
